ea8c551a54f6b7e6a14a09cfa1264047d847996e
Mazeli Victor <victor.vic.mazeli@gmail.com>
2020-04-13 17:59:49 +0100

    indentation updated

--- src/estimator.py
+++ src/estimator.py
@@ -1,12 +1,12 @@
 def estimator(data):
+
   # ASSESSMENT BEGINING
 
  currentlyInfectedImpact = int(data.get(reportedCases) * 10)
-
  currentlyInfectedSevere = int(data.get(reportedCases) * 50)
 
-  # CHALLENGE 1
-
+ # CHALLENGE 1
+ 
  def covid19ImpactEstimator():
    global currentlyInfectedImpact
    time = data.get(timeElapse)
@@ -14,50 +14,53 @@ def estimator(data):
    im_value = int(currentlyInfectedImpact * (2 ^ factor))
    return im_value
   
-  infectionByRequestedTimeImpact = covid19ImpactEstimator()
-
-   def covid19SeveretEstimator():
-   global currentlyInfectedSevere
-   time = data.get(timeElapse)
-   factor = time / 3
-   s_value = int(currentlyInfectedSevere * (2 ^ factor))
-   return s_value
   
-  infectionByRequestedTimeSevere = covid19SevereEstimator()
+  
+infectionByRequestedTimeImpact = covid19ImpactEstimator()
+
+def covid19SeveretEstimator():
+  global currentlyInfectedSevere
+  time = data.get(timeElapse)
+  factor = time / 3
+  s_value = int(currentlyInfectedSevere * (2 ^ factor))
+  return s_value
+
+infectionByRequestedTimeSevere = covid19SevereEstimator()
 
   # CHALLENGE 2
 
-  severeCaseByRequestedTimeImpact = int(infectionByRequestedTimeImpact * 0.15)
+severeCaseByRequestedTimeImpact = int(infectionByRequestedTimeImpact * 0.15)
 
-  severeCaseByRequestedTimeSevere = int(infectionByRequestedTimeSevere * 0.15)
+severeCaseByRequestedTimeSevere = int(infectionByRequestedTimeSevere * 0.15)
 
-  def hospitalBedByrequestedImpact():
-    global severeCaseByRequestedTimeImpact
-    hos_im_value  = data.get(totalHospitalBeds) * 0.35
-    im_available_beds = int(severeCaseByRequestedTimeImpact - hos_im_value)
-    return im_available_beds
-  
-  hospitalBedByrequestedTimeImpact = hospitalBedByrequestedImpact()
+def hospitalBedByrequestedImpact():
+  global severeCaseByRequestedTimeImpact
+  hos_im_value  = data.get(totalHospitalBeds) * 0.35
+  im_available_beds = int(severeCaseByRequestedTimeImpact - hos_im_value)
+  return im_available_beds
+   
+     
+hospitalBedByrequestedTimeImpact = hospitalBedByrequestedImpact()
 
-  def hospitalBedByrequestedSevere():
+ def hospitalBedByrequestedSevere():
     global severeCaseByRequestedTimeSevere
     hos_s_value  = data.get(totalHospitalBeds) * 0.35
     s_available_beds = int(severeCaseByRequestedTimeSevere - hos_s_value)
     return s_available_beds
 
-   hospitalBedByrequestedTimeSevere = hospitalBedByrequestedSevere()
+hospitalBedByrequestedTimeSevere = hospitalBedByrequestedSevere()
 
   #  CHALLENGE 3
 
-  casesForICUByRequestedTimeImpact = int(infectionByRequestedTimeImpact * 0.05)
+casesForICUByRequestedTimeImpact = int(infectionByRequestedTimeImpact * 0.05)
 
-  casesForICUByRequestedTimeSevere = int(infectionByRequestedTimeSevere * 0.05)
+casesForICUByRequestedTimeSevere = int(infectionByRequestedTimeSevere * 0.05)
 
-  casesForVentilatorsByrequestedTimeImpact = int(infectionByRequestedTimeImpact * 0.02)
+casesForVentilatorsByrequestedTimeImpact = int(infectionByRequestedTimeImpact * 0.02)
 
-  casesForVentilatorsByrequestedTimeSevere = int(infectionByRequestedTimeSevere * 0.02)
+casesForVentilatorsByrequestedTimeSevere = int(infectionByRequestedTimeSevere * 0.02)
 
-  def dollarImpact():
+def dollarImpact():
     global infectionByRequestedTimeImpact
     daily_income_impact = data.get(avgDailyIncomeInUSD)
     population_impact = data.get(avgDailyIncomePopulation)
@@ -65,9 +68,9 @@ def estimator(data):
     compute_impact = int((infectionByRequestedTimeImpact * population_impact * daily_income_impact) / period_impact)
     return compute_impact
 
-  dollarsInFlightImpact = dollarImpact()
+dollarsInFlightImpact = dollarImpact()
 
-  def dollarSevere():
+def dollarSevere():
     global infectionByRequestedTimeSevere
     daily_income_severe = data.get(avgDailyIncomeInUSD)
     population_severe = data.get(avgDailyIncomePopulation)
@@ -75,21 +78,20 @@ def estimator(data):
     compute_severe = int((infectionByRequestedTimeSevere * population_severe * daily_income_severe) / period_severe)
     return compute_severe
 
-  dollarsInFlightSevere = dollarSevere()
+dollarsInFlightSevere = dollarSevere()
 
+# OUTPUTS
 
-  # OUTPUTS
+data = {
 
   data = {
-
-    data = {
 
     'data': data
   }
 
- estimate = {
+estimate = {
 
-    impact = {
+  impact = {
 
      'currentlyInfected': currentlyInfectedImpact,
      'infectionByRequestedTime': infectionByRequestedTimeImpact,
@@ -118,6 +120,9 @@ def estimator(data):
 
 
   }
+  
+
+
   
   return data
 
